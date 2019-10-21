a676937a96e64fe88c6e81564288145c6e2ba91f
Goay Eng Yew <vinc3nt86@hotmail.com>
2019-10-21 14:40:57 +0800

    update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, () => {
     console.log(`AppServer is running on port ${PORT}`);
 })
